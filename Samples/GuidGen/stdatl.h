015bb71722efa3bd8a448a41981d0cb4a1019653
nenadstefanovic <nenadstefanovic@users.sf.net>
2018-01-11 01:15:55 -0800

    Samples: Cleanup, updated to work properly with WTL10, used new WTL icon, removed unusd files

--- Samples/GuidGen/stdatl.h
+++ Samples/GuidGen/stdatl.h
@@ -7,7 +7,7 @@
 #define WINVER		0x0501
 #define _WIN32_WINNT	0x0501
 #define _WIN32_IE	0x0600
-#define _RICHEDIT_VER	0x0200
+#define _RICHEDIT_VER	0x0300
 
 #include <atlbase.h>
 #include <atlapp.h>
